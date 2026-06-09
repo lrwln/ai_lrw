9129dbe72de429eead72b9409b3b0b0e6237ce0c
lrwln <2766394102@qq.com>
2026-06-09 23:03:45 +0800

    new files

---
+++ ai/wnd/nlp-demo/client.mjs
@@ -0,0 +1,13 @@
+// 负责 提供llm client 对象
+import { OpenAI } from "openai/client.js";
+import dotenv from "dotenv";
+
+
+dotenv.config();// 读取环境变量 即读取 .env 文件中的内容
+const client = new OpenAI({
+    apiKey: process.env.DEEPSEEK_API_KEY,
+    baseURL: process.env.DEEPSEEK_API_BASE_URL,
+});
+// export const a = 2;//直接导出
+// export const b = 3;//
+export default client;//默认导出
---
+++ ai/wnd/nlp-demo/main.mjs
@@ -0,0 +1,3 @@
+// 主程序文件
+import client from './client.mjs';
+console.log(client);
